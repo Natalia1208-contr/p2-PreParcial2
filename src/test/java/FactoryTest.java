03e382a09654756ae484eba114a3ed4a966be851
Juan Miguel Gutierrez V. <munekoporsiempre2017@gmail.com>
2026-04-22 10:54:13 -0500

    add test factory

--- src/test/java/FactoryTest.java
+++ src/test/java/FactoryTest.java
@@ -9,5 +9,10 @@ class FactoryTest {
         Cancion c1 = (Cancion)fabrica.crearContenido("1", "Oh que sera", 3, "Willie Colon", "Salsa", EstadoContenido.DISPONIBLE);
         assertEquals("Oh que sera", c1.getTitulo());
     }
-
+    @Test
+    void tipoCorrecto(){
+        ContFactory fabrica = new CancionFact();
+        Cancion c1 = (Cancion)fabrica.crearContenido("1", "Oh que sera", 3, "Willie Colon", "Salsa", EstadoContenido.DISPONIBLE);
+        assertTrue(c1 instanceof Cancion);
+    }
 }
